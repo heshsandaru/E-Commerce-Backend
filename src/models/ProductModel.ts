5b3ff0ed592819d4d3529e225d5ef6797cf323f6
Heshan <heshansandaruwan999@gmail.com>
2025-12-14 21:16:39 +0530

    product model add

---
+++ src/models/ProductModel.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Product name is required"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Product description is required"],
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+        min: [0, "Price cannot be negative"],
+    },
+    stock: {
+        type: Number,
+        required: [true, "Product stock is required"],
+        min: [0, "Stock cannot be negative"],
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+    }        
+})
+
+const ProductModel = mongoose.model("Product", productSchema);
+
+export { ProductModel };
