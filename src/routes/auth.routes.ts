29212c28bf3ec554a2e1e03f1d8bf49bf0dbde69
Heshan <heshansandaruwan999@gmail.com>
2025-12-14 20:15:24 +0530

    done full signup fuction and db connect

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { signup } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
+authRouter.post("/signup", signup);
 
+
+export default authRouter;
--- src/routes/root.router.ts
+++ src/routes/root.router.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import authRouter from "./auth.routes";
 
 const rootRouter = Router();
+
+rootRouter.use("/auth", authRouter);
 
 export default rootRouter;
