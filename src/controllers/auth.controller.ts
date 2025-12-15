3da2ba004561ecfd6d2e1f5b09780d7debc932d4
Heshan <heshansandaruwan999@gmail.com>
2025-12-15 19:49:05 +0530

    update json token refresh

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt"
 import { Request, Response, NextFunction } from "express"
 import { UserModel } from "../models/UserModel"
-import jwt from "jsonwebtoken"
+import jwt, { JsonWebTokenError, JwtPayload, TokenExpiredError } from "jsonwebtoken"
 import { APIError } from "../errors/ApiError"
 
 const createAccessToken = (userId: string) => {
@@ -69,6 +69,64 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     }
     res.status(200).json(userWithoutPassword)
   } catch (err: any) {
+    next(err)
+  }
+}
+
+export const refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies?.refreshToken
+    if (!token) {
+      throw new APIError(401, "Refresh token missing")
+    }
+
+    jwt.verify(
+      token,
+      process.env.REFRESH_TOKEN_SECRET!,
+      async (err: Error | null, decoded: string | JwtPayload | undefined) => {
+        if (err) {
+          if (err instanceof TokenExpiredError) {
+            throw new APIError(401, "Refresh token expired")
+          } else if (err instanceof JsonWebTokenError) {
+            throw new APIError(401, "Invalid refresh token")
+          } else {
+            throw new APIError(401, "Refresh token error")
+          }
+        }
+
+        if (!decoded || typeof decoded === "string") {
+          throw new APIError(401, "Refresh token payload error")
+        }
+
+        const userId = decoded.userId as string
+        const user = await UserModel.findById(userId)
+
+        if (!user) {
+          throw new APIError(401, "User not found")
+        }
+
+        const newAccessToken = createAccessToken(user._id.toString())
+        res.status(200).json({ accessToken: newAccessToken })
+      }
+    )
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const logout = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const isProd = process.env.NODE_ENV === "production"
+
+    res.cookie("refreshToken", "", {
+      httpOnly: true,
+      secure: isProd,
+      expires: new Date(0),
+      path: "/api/auth/refresh-token",
+    })
+
+    res.status(200).json({ message: "Logout successful" })
+  } catch (err) {
     next(err)
   }
 }
